d544d77851fdc68b5826897bb71951ebcc6a9d52
ducit488 <ducit488@gmail.com>
2023-09-14 11:16:59 +0700

    create user model

---
+++ src/main/java/Modell/User.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modell;
+
+/**
+ *
+ * @author admin
+ */
+public class User {
+    public String id;
+    public String name;
+    public int age;
+
+    public User(String id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+ 
+    
+}
