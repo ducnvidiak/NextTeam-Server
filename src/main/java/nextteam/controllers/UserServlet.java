42660def8c0b5b0b5c4869e61806a83478788b2a
Bravee06 <bravee300@gmail.com>
2023-10-24 23:45:41 +0700

    final upload

--- src/main/java/nextteam/controllers/UserServlet.java
+++ src/main/java/nextteam/controllers/UserServlet.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -18,6 +19,7 @@ import nextteam.models.User;
  *
  * @author bravee06
  */
+@WebServlet(name = "UserServlet", urlPatterns = {"/api/user_manager"})
 public class UserServlet extends HttpServlet {
 
     @Override
