2adf9dd39caab2ade98ad124fd0b6378cab14ffb
ducit488 <ducit488@gmail.com>
2023-10-24 22:27:55 +0700

    fix callback

--- src/main/java/nextteam/Global.java
+++ src/main/java/nextteam/Global.java
@@ -40,7 +40,7 @@ public class Global {
     public static String server = "localhost";
     public static String database = "NextTeam";
     public static String username = "sa";
-    public static String password = "123456aA@$";
+    public static String password = "1";
     private static Connection conn;
     public static String workingPath;
     public static ClubDAO clubDAO;
