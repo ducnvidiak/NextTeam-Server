669914656dca56c58f72e9a11aaef7c17bfde945
Ilnung Terl anofp Drohelviaskhf <vnitd.owner@gmail.com>
2023-11-08 10:02:38 +0700

    fix google authentication 1

--- src/main/java/nextteam/controllers/Constants.java
+++ src/main/java/nextteam/controllers/Constants.java
@@ -9,15 +9,16 @@ package nextteam.controllers;
  * @author baopg
  */
 public class Constants {
+
     public static String GOOGLE_CLIENT_ID = "314493880440-he0s6oe3g6rt0lth4k7q2t7n5pjdk75e.apps.googleusercontent.com";
 
-	public static String GOOGLE_CLIENT_SECRET = "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";
+    public static String GOOGLE_CLIENT_SECRET = "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";
 
-	public static String GOOGLE_REDIRECT_URI = "http://localhost:8080/login-google";
+    public static String GOOGLE_REDIRECT_URI = "https://nextteam.azurewebsites.net/login-google";
 
-	public static String GOOGLE_LINK_GET_TOKEN = "https://accounts.google.com/o/oauth2/token";
+    public static String GOOGLE_LINK_GET_TOKEN = "https://accounts.google.com/o/oauth2/token";
 
-	public static String GOOGLE_LINK_GET_USER_INFO = "https://www.googleapis.com/oauth2/v1/userinfo?access_token=";
+    public static String GOOGLE_LINK_GET_USER_INFO = "https://www.googleapis.com/oauth2/v1/userinfo?access_token=";
 
-	public static String GOOGLE_GRANT_TYPE = "authorization_code";
+    public static String GOOGLE_GRANT_TYPE = "authorization_code";
 }
